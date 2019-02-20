1abd59fa255f473658f80eacc03e85d90de87ef8
mikilanjlo <mikilanjlo@yandex.by>
2019-02-20 10:43:26 +0300

    calculate

--- StringExtension.cs
+++ StringExtension.cs
@@ -6,8 +6,26 @@ namespace PadawansTask7
     {
         public static void OrderStringsByLength(string[] array)
         {
-            // put your code here
-            throw new NotImplementedException();
+            if (array == null)
+                throw new ArgumentNullException();
+            if (array.Length <= 0)
+                throw new ArgumentException();
+
+            StringLengthSort(array);
+        }
+
+        private static void StringLengthSort(string[] A)
+        {
+            for (int i = 0; i < A.Length; i++)
+                for (int j = 0; j < A.Length - 1; j++)
+                {
+                    if (A[j].Length > A[j + 1].Length)
+                    {
+                        string temp = A[j];
+                        A[j] = A[j + 1];
+                        A[j + 1] = temp;
+                    }
+                }
         }
     }
 }
